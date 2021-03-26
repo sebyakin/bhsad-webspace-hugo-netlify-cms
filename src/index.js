98a505c46eb72f576e1a9d57fbf1adc9456a70e5
sebyakin <46461635+sebyakin@users.noreply.github.com>
2021-03-26 15:47:57 +0300

    hi

--- src/index.js
+++ src/index.js
@@ -2,5 +2,28 @@
 
 import "./css/style.scss";
 
-// Say hello
-console.log("🦊 Hello! Edit me in src/index.js");
+// alert('Welcome, dear visitor 😬')
+
+let body = document.body;
+let checkThis = document.querySelector('.check-this');
+
+checkThis.addEventListener('mouseover', function(){
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    body.style.setProperty('background-color', 'rgb('
+    +r+ ',' +g+ ',' +b+ ')');
+})
+
+function rotate() {
+    let all = document.body.querySelectorAll('*');
+    let x = Math.floor(Math.random() * 40);
+
+    for (each of all) {
+        each.style.setProperty('transform', 'rotateZ(' +x+ 'deg)' );
+    }
+}
+
+// setTimeout(function() {rotate()}, 3000); 
+
+document.addEventListener('mousemove', rotate);
